c44f884d3aa1adb1ffa37917f28ab11f5bf52a1d
GuilhermeHenrique659 <guilhermehenrique.o@hotmail.com>
2024-02-16 21:38:26 -0300

    add save temp file to classify

--- src/common/providers/LocalAttachmentProvider.ts
+++ src/common/providers/LocalAttachmentProvider.ts
@@ -1,7 +1,7 @@
 import Attachment from "@modules/attachment/domain/entity/Attachment";
 import IAttachmentProvider from "./IAttachmentProvider";
 import updateConfig from "@config/UpdateConfig";
-
+import fs from 'fs'
 
 class LocalAttachmentProvider implements IAttachmentProvider {
     private readonly filenames: string[] = []
@@ -11,7 +11,7 @@ class LocalAttachmentProvider implements IAttachmentProvider {
     public async save(data: string): Promise<Attachment> {
         const mimeType = '.jpg'
         const filename = updateConfig.storage.filename() + mimeType;
-
+        await fs.promises.writeFile(`${updateConfig.storage.directory}/${filename}`, data, 'base64');
         return Attachment.createAttachment({
             fileName: filename,
             data,
