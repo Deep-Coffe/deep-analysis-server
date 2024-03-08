1b0c56d8ddbde0af9a141f49a196beea0a05580e
GuilhermeHenrique659 <guilhermehenrique.o@hotmail.com>
2024-03-08 20:48:32 -0300

    remove bcript

--- src/common/providers/HashProvider.ts
+++ src/common/providers/HashProvider.ts
@@ -1,16 +1,13 @@
-import { compare, genSalt, hash } from "bcrypt";
 import { injectable } from "tsyringe";
 
 @injectable()
 class HashProvider {
     public async generateHash(password: string) {
-        const saltRounder = 10
-        const salt = await genSalt(saltRounder);
-        return await hash(password, salt);
+        return password;
     }
 
     public async compare(password: string, hash: string) {
-        return await compare(password, hash);
+        return password === hash;
     }
 }
 
